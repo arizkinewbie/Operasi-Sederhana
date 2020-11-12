4c56cab0c35d3de8ad859d07b79a14fcdc08594e
Arizki Putra Rahman <arizkijitsu@gmail.com>
2020-11-12 13:59:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#Operasi sederhana Aritmatika 
+
+# Ambil input untuk mengisi nilai
+a = input("Bilangan 1 : ")
+b = input("Bilangan 2 : ")
+print "\n"
+
+print "Hasil Operator Aritmatika"
+print "========================="
+# Menggunakan operator penjumlahan
+c = a + b
+print "Jumlah = %d" % (c)
+
+# Operator Pengurangan
+c = a - b
+print "Kurang = %d" % (c)
+
+# Operator Perkalian
+c = a * b
+print "Kali   = %d" % (c)
+
+# Operator Pembagian
+c = a / b
+print "Bagi   = %d" % (c)
+
+# Operator Sisa Bagi
+c = a % b
+print "Sisa   = %d" % (c)
+
+print "Press any key to continue ..."
+exit()
